38f4a09300124b9ec0af1be9975cc6fbf8834f68
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2025-11-02 08:12:01 +0200

    Continues on setup helper

--- src/main/java/fi/tuomax/codingpuzzles/everybodycodes/setuphelpers/SetupEcYear.java
+++ src/main/java/fi/tuomax/codingpuzzles/everybodycodes/setuphelpers/SetupEcYear.java
@@ -1,7 +1,11 @@
 package fi.tuomax.codingpuzzles.everybodycodes.setuphelpers;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 
 public class SetupEcYear 
@@ -12,6 +16,10 @@ public class SetupEcYear
     private static Integer year;
 
     private static Integer day;
+
+    private static String name;
+
+    private static String namePascalCase;
 
     private static String workingDir = "";
 
@@ -28,20 +36,40 @@ public class SetupEcYear
 
         System.out.print("Year> ");
         year = scanner.nextInt();
+        scanner.nextLine();
 
         System.out.print("Quest> ");
         day = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Name> ");
+        name = scanner.nextLine();
 
         clazzPath = String.format(
             "fi.tuomax.codingpuzzles.everybodycodes.events.year%d.quest%d",
             year, day
         );
 
+        pascalizeName();
         createInputDir();
         createSourceDir();
+        createSolver(1);
+        createSolver(2);
+        createSolver(3);
         createTestDir();
 
         scanner.close();
+    }
+
+    private static void pascalizeName()
+    {
+        String[] parts = name.split(" |-");
+        StringBuilder pascalized = new StringBuilder();
+        for (String part : parts) {
+            pascalized.append(part.substring(0, 1).toUpperCase());
+            pascalized.append(part.substring(1).toLowerCase());
+        }
+        namePascalCase = pascalized.toString();
     }
 
     private static void createInputDir() 
@@ -93,6 +121,41 @@ public class SetupEcYear
             String.format("year%d", year),
             String.format("quest%d", day)
         ).toString();
+    }
+
+    private static void createSolver(Integer part)
+    {
+        Path solverSrc = Path.of(
+            getSourceDir(),
+            String.format("%s_Solver_Part%d.java", namePascalCase, part)
+        );
+        String[] lines = new String[]{
+            "package " + clazzPath + ";",
+            "",
+            "import fi.tuomax.codingpuzzles.framework.Solver;",
+            "",
+            "public class " + namePascalCase + "_Solver_Part" + part,
+            "extends Solver",
+            "{",
+            "",
+            "    @Override",
+            "    public void solve(List<String> input) ",
+            "    {",
+            "    }",
+            "    ",
+            "}",
+        };
+        FileWriter writer = null;
+        try {
+            writer = new FileWriter(solverSrc.toFile());
+            for (String line : lines) {
+                writer.write(line);
+                writer.write("\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Unable to write to " + solverSrc.toString() + ".");
+        } 
     }
 
     private static void createTestDir() 
