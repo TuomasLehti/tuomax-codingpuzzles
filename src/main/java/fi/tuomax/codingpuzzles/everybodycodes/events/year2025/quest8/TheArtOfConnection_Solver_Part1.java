4c145a9f552212fa047dbf7750e7792a33eb2d0d
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2025-11-14 00:46:41 +0200

    Extracts parsing to a method for code reuse

--- src/main/java/fi/tuomax/codingpuzzles/everybodycodes/events/year2025/quest8/TheArtOfConnection_Solver_Part1.java
+++ src/main/java/fi/tuomax/codingpuzzles/everybodycodes/events/year2025/quest8/TheArtOfConnection_Solver_Part1.java
@@ -2,23 +2,37 @@ package fi.tuomax.codingpuzzles.everybodycodes.events.year2025.quest8;
 
 import fi.tuomax.codingpuzzles.common.IntegerList;
 import fi.tuomax.codingpuzzles.framework.Solver;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class TheArtOfConnection_Solver_Part1
 extends Solver
 {
 
+    protected List<Integer> seq = null;
+
+    protected List<Chord> chords = null;
+
     @Override
     public void solve(List<String> input) 
     {
-        List<Integer> seq = IntegerList.fromString(input.get(0), ",");
-        Chord.NUM_OF_NAILS = jsonMetadata.getInt("numOfNails");
-
+        parse(input);
         int ans = 0;
-        for (int i = 0; i < seq.size()-1; i++) 
-            if (new Chord(seq.get(i), seq.get(i + 1)).isDiameter())
+        for (Chord chord : chords) 
+            if (chord.isDiameter())
                 ans++;
         setAnswer(Integer.toString(ans));
+    }
+
+    protected void parse(List<String> input)
+    {
+        Chord.NUM_OF_NAILS = jsonMetadata.getInt("numOfNails");
+        seq = IntegerList.fromString(input.get(0), ",");
+        chords = new ArrayList<>();
+        for (int i = 0; i < seq.size() - 1; i++) {
+            chords.add(new Chord(seq.get(i), seq.get(i + 1)));
+        }
     }
     
 }
--- src/main/java/fi/tuomax/codingpuzzles/everybodycodes/events/year2025/quest8/TheArtOfConnection_Solver_Part2.java
+++ src/main/java/fi/tuomax/codingpuzzles/everybodycodes/events/year2025/quest8/TheArtOfConnection_Solver_Part2.java
@@ -1,25 +1,15 @@
 package fi.tuomax.codingpuzzles.everybodycodes.events.year2025.quest8;
 
-import fi.tuomax.codingpuzzles.common.IntegerList;
-import fi.tuomax.codingpuzzles.framework.Solver;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class TheArtOfConnection_Solver_Part2
-extends Solver
+extends TheArtOfConnection_Solver_Part1
 {
 
     @Override
     public void solve(List<String> input) 
     {
-        Chord.NUM_OF_NAILS = jsonMetadata.getInt("numOfNails");
-        List<Chord> chords = new ArrayList<>();
-        List<Integer> seq = IntegerList.fromString(input.get(0), ",");
-        for (int i = 0; i < seq.size() - 1; i++) {
-            chords.add(new Chord(seq.get(i), seq.get(i + 1)));
-        }
-
+        parse(input);
         int knots = 0;
         for (int i = 1; i < chords.size(); i++)
             for (int j = 0; j < i; j++)
