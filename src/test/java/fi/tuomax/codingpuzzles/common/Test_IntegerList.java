2828ea63135110f597f8863269486cf2a6b1e2c6
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2025-11-16 20:46:03 +0200

    Adds tests for IntegerList

---
+++ src/test/java/fi/tuomax/codingpuzzles/common/Test_IntegerList.java
@@ -0,0 +1,33 @@
+package fi.tuomax.codingpuzzles.common;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class Test_IntegerList 
+{
+
+    @Test
+    public void test_fromString()
+    {
+        List<Integer> ints = IntegerList.fromString("1,2,3,4,-5,-6", ",");
+        assertEquals(6, ints.size());
+        assertEquals(1, (int) ints.get(0));
+        assertEquals(2, (int) ints.get(1));
+        assertEquals(3, (int) ints.get(2));
+        assertEquals(4, (int) ints.get(3));
+        assertEquals(-5, (int) ints.get(4));
+        assertEquals(-6, (int) ints.get(5));
+    }
+
+    @Test
+    public void test_removeDuplicates()
+    {
+        List<Integer> ints = IntegerList.fromString("1,2,3,2,3,4", ",");
+        List<Integer> uniqueInts = IntegerList.removeDuplicates(ints);
+        assertEquals(4, uniqueInts.size());
+    }
+    
+}
