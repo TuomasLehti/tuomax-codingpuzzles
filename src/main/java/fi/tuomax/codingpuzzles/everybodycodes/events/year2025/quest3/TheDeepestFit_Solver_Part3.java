86580fe5c5bd42e2629672ffa1bb4ea44a8edb55
Tuomas Lehti <tuomas.a.lehti@gmail.com>
2025-11-17 11:16:13 +0200

    EC 2025, quest 3, part 3 now uses Counter

--- src/main/java/fi/tuomax/codingpuzzles/everybodycodes/events/year2025/quest3/TheDeepestFit_Solver_Part3.java
+++ src/main/java/fi/tuomax/codingpuzzles/everybodycodes/events/year2025/quest3/TheDeepestFit_Solver_Part3.java
@@ -1,10 +1,10 @@
 package fi.tuomax.codingpuzzles.everybodycodes.events.year2025.quest3;
 
+import fi.tuomax.codingpuzzles.common.Counter;
+import fi.tuomax.codingpuzzles.common.IntegerList;
 import fi.tuomax.codingpuzzles.framework.Solver;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class TheDeepestFit_Solver_Part3
 extends Solver
@@ -13,23 +13,10 @@ extends Solver
     @Override
     public void solve(List<String> input) 
     {
-        Map<Integer, Integer> crates = parse(input);
-        Integer mostRepeats = Integer.MIN_VALUE;
-        for (Integer size : crates.keySet()) {
-            mostRepeats = Math.max(mostRepeats, crates.get(size));
-        }
-        setAnswer(mostRepeats.toString());
-    }
-
-    private Map<Integer, Integer> parse(List<String> input) 
-    {
-        Map<Integer, Integer> crates = new HashMap<>();
-        String[] parts = input.get(0).split(",");
-        for (String part : parts) {
-            Integer partAsInt = Integer.valueOf(part);
-            crates.put(partAsInt, crates.getOrDefault(partAsInt, 0) + 1);
-        }
-        return crates;
+        List<Integer> crates = IntegerList.fromString(input.get(0), ",");
+        Counter<Integer> counter = new Counter<>();
+        counter.countAll(crates);
+        setAnswer(counter.getMaxAmount());
     }
     
 }
